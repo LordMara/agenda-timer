e39fa7abe4d228ca0a627c6b017a03ec94cdeb10
abystry <arek.bystrowicz@gmail.com>
2018-04-18 16:25:59 +0200

    Add static method renderTime for parsing time from number(milis) to string

--- resources/js/model/Timer.js
+++ resources/js/model/Timer.js
@@ -6,4 +6,21 @@ export class Timer {
         this._mainClock = mainClock;
         this._timer = null;
     }
+
+    static renderTime(time) {
+        let s = time/1000;
+        let h = 0;
+        let m = 0;
+
+        while (s >= 60) {
+            s -= 60;
+            m += 1;
+        }
+
+        while (m >= 60) {
+            m -= 60;
+            h += 1;
+        }
+        return `${h}h ${m}m ${s}s`
+    }
 }
