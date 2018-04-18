7b60f5bc1225a82bb7c463b678f408d7ff97efd9
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2018-04-18 14:32:50 +0200

    Implement app.js first version for test purpose

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,3 +2,21 @@
 
 import {Event} from "./model/Event.js";
 import {Meeting} from "./model/Meeting.js";
+import {NewButtonController} from "./addmeeting/NewMeetingButtonsController.js"
+import {NewMeetingsButtonsView} from "./addmeeting/NewMeetingsButtonsView.js";
+import {FormView} from "./addmeeting/FormView.js";
+import {FormController} from "./addmeeting/FormController.js";
+
+
+let meeting = new Meeting(1 , "test", new Array());
+let buttonsController = new NewButtonController(meeting);
+let buttonsView = new NewMeetingsButtonsView(buttonsController);
+
+// load existing meetings from local storage
+
+// load first form
+let event = new Event();
+let formController = new FormController(event, meeting);
+let formView = new FormView(formController);
+let container = document.querySelector("#form-container-inner");
+container.appendChild(formView.element);
