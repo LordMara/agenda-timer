aafe7b89d90dffdf0bcd26351750b318e71d6ba4
abystry <arek.bystrowicz@gmail.com>
2018-04-18 16:28:24 +0200

    Add methods responsible of controlling Timer attribiutes changes

--- resources/js/model/Timer.js
+++ resources/js/model/Timer.js
@@ -7,6 +7,24 @@ export class Timer {
         this._timer = null;
     }
 
+    countdown() {
+        this._time -= 1000;
+        this._mainTime -= 1000;
+    }
+
+    setClocks() {
+        this._clock.innerHTML = Timer.renderTime(this._time);
+        this._mainClock.innerHTML = Timer.renderTime(this._mainTime);
+    }
+
+    isRunning() {
+        return this._time > 0;
+    }
+
+    hasFinished() {
+        return this._time <= 0;
+    }
+
     static renderTime(time) {
         let s = time/1000;
         let h = 0;
