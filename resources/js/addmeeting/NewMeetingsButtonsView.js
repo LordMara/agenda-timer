da2ba0597cb5da4cfe75adc55fd4cdcce8d075be
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2018-04-19 09:37:55 +0200

    Add event listener for submit button with callback handleSubmit()

--- resources/js/addmeeting/NewMeetingsButtonsView.js
+++ resources/js/addmeeting/NewMeetingsButtonsView.js
@@ -5,6 +5,9 @@ export class NewMeetingsButtonsView {
     }
 
     registerEventListeners() {
-        document.querySelector("#add-event").addEventListener("click", this.controller.handleAddEvent.bind(this.controller));
+        document.querySelector("#add-event")
+            .addEventListener("click", this.controller.handleAddEvent.bind(this.controller));
+        document.querySelector("#add-new-meeting")
+            .addEventListener("click", this.controller.handleSubmit.bind(this.controller));
     }
 }
