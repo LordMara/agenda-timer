216f0c0c75f47db7f18c9472c8649278d026b0b2
abystry <arek.bystrowicz@gmail.com>
2018-04-19 10:41:48 +0200

    Add view and document build to controller

--- resources/js/controller/TimerController.js
+++ resources/js/controller/TimerController.js
@@ -1,13 +1,35 @@
 import {Timer} from "../model/Timer.js";
+import {MeetingView} from "../view/MeetingView.js";
 
 export class TimerController {
     constructor(meeting) {
-        this._timer = TimerController.createTimer(meeting);
+        this._meeting = meeting;
+        this._view = new MeetingView(meeting);
+        this._timer = null;
+    }
+
+    build() {
+        this._view.createElement();
+        this.createTimer();
+    }
+
+    createTimer() {
+        let meetingTime = this._meeting.estimatedTime();
+        let meetingClock = document.getElementById('meeting').getElementsByTagName('p')[1];
+        let eventsTimes = [];
+        let eventsClocks = [];
+
+        for (let i = 0; i < this._meeting.events.length; i++) {
+            eventsTimes.push(this._meeting.events[i].time);
+            eventsClocks.push(document.getElementsByTagName('li')[i].getElementsByTagName('p')[1]);
+        }
+
+        this._timer = new Timer(meetingTime, meetingClock, eventsTimes, eventsClocks);
     }
 
     start() {
         let timer = this._timer;
-        let stopper = setInterval(function () {
+        let stopper = setInterval(function() {
             timer.countdown();
             timer.setClocks();
 
@@ -19,20 +41,5 @@ export class TimerController {
                 clearInterval(stopper);
             }
         }, 1000);
-    }
-
-    static createTimer(meeting) {
-        let eventsTimes = [];
-        let eventsClocks = [];
-
-        for (let i = 0; i < meeting.events.length; i++) {
-            eventsTimes.push(meeting.events[i].time);
-            eventsClocks.push(document.getElementsByTagName('li')[i].getElementsByTagName('p')[1]);
-        }
-
-        let meetingTime = meeting.estimatedTime();
-        let meetingClock = document.getElementById('meeting').getElementsByTagName('p')[1];
-
-        return new Timer(meetingTime, meetingClock, eventsTimes, eventsClocks);
     }
 }
