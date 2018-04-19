303a93c627fa80112a50ab3b7e955211251a27d2
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2018-04-19 11:34:43 +0200

    Add fields _hours and _minutes with getters and setters, add handleHoursChange() and handleMinutesChange() methods

--- resources/js/addmeeting/eventform/EventFormController.js
+++ resources/js/addmeeting/eventform/EventFormController.js
@@ -3,13 +3,40 @@ export class EventFormController {
         this.event = event;
         this.meeting = meeting;
         this.meeting.addEvent(event);
+        this._hours = 0;
+        this._minutes = 0;
+    }
+
+    get hours() {
+        return this._hours;
+    }
+
+    set hours(value) {
+        this._hours = value;
+    }
+
+    get minutes() {
+        return this._minutes;
+    }
+
+    set minutes(value) {
+        this._minutes = value;
     }
 
     handleEventTitleChange(e) {
         this.event.topic = e.target.value;
     }
 
-    handleEventTimeChange(e) {
-        this.event.time = e.target.value;
+    handleHoursChange(e) {
+        this.event.time -= this._hours * 36 * 1000 * 1000;
+        this._hours = parseInt(e.target.value);
+        this.event.time += this._hours * 36 * 1000 * 1000;
+        console.log(this);
+    }
+
+    handleMinutesChange(e) {
+        this.event.time -= this._minutes * 60 * 1000;
+        this._minutes = parseInt(e.target.value);
+        this.event.time += this._minutes * 60 * 1000;
     }
 }
