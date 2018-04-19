cfad87dfb1fdbbf4e7be8f239992341c9d54325f
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2018-04-19 10:17:48 +0200

    Add creation of input for meeting name

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,12 +2,27 @@
 
 import {Event} from "./model/Event.js";
 import {Meeting} from "./model/Meeting.js";
+
 import {NewButtonController} from "./addmeeting/NewMeetingButtonsController.js"
 import {NewMeetingsButtonsView} from "./addmeeting/NewMeetingsButtonsView.js";
+
 import {EventFormView} from "./addmeeting/eventform/EventFormView.js";
 import {EventFormController} from "./addmeeting/eventform/EventFormController.js";
 
+import {MeetingFormView} from "./addmeeting/meetingform/MeetingFormView.js";
+import {MeetingFormController} from "./addmeeting/meetingform/MeetingFormController.js";
+
+
+
+// get id base on local storage here
 let meeting = new Meeting(1 , "test", new Array());
+
+// create form for meeting name
+let meetingFormController = new MeetingFormController(meeting);
+let meetingFormView = new MeetingFormView(meetingFormController);
+let meetingContainer = document.querySelector("#form-container");
+meetingContainer.insertBefore(meetingFormView.element, meetingContainer.firstChild);
+
 let buttonsController = new NewButtonController(meeting);
 let buttonsView = new NewMeetingsButtonsView(buttonsController);
 
@@ -15,7 +30,7 @@ let buttonsView = new NewMeetingsButtonsView(buttonsController);
 
 // load first form
 let event = new Event();
-let formController = new EventFormController(event, meeting);
-let formView = new EventFormView(formController);
-let container = document.querySelector("#form-container-inner");
-container.appendChild(formView.element);
+let eventFormController = new EventFormController(event, meeting);
+let eventFormView = new EventFormView(eventFormController);
+let eventContainer = document.querySelector("#form-container-inner");
+eventContainer.appendChild(eventFormView.element);
